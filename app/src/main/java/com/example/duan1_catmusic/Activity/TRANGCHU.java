7a3dad67fea4a20360722292eb34dee9a578463e
Tanniiee <tannie1101999@gmail.com>
2024-07-13 21:49:04 +0700

    Thu Thao

--- app/src/main/java/com/example/duan1_catmusic/Activity/TRANGCHU.java
+++ app/src/main/java/com/example/duan1_catmusic/Activity/TRANGCHU.java
@@ -1,26 +1,77 @@
 package com.example.duan1_catmusic.Activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.duan1_catmusic.R;
+import com.example.duan1_catmusic.fragment.Premium_fm;
+import com.example.duan1_catmusic.fragment.Thuvien_fm;
+import com.example.duan1_catmusic.fragment.Tim_kiem_fm;
+import com.example.duan1_catmusic.fragment.Trangchu_fm;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class TRANGCHU extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.gd_trangchu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.navHome) {
+                    loadFragment(new Trangchu_fm(), false);
+                } else if (itemId == R.id.navTimKiem) {
+                    loadFragment(new Tim_kiem_fm(), false);
+                } else if (itemId == R.id.navThuVien) {
+                    loadFragment(new Thuvien_fm(), false);
+                } else {
+                    loadFragment(new Premium_fm(), false);
+                }
+                return true;
+            }
         });
+
+        loadFragment(new Trangchu_fm(), true);
+    }
+
+    private void loadFragment(Fragment fragment, boolean isAppInitialized) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (isAppInitialized) {
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+        fragmentTransaction.commit();
+
+
+
     }
 }
