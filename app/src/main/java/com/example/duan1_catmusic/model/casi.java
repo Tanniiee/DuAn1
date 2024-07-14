d59b2180e59508069142e95bacb4ec8afcc93df4
Bikerrrr <hieuttps37551@edu.fpt.vn>
2024-07-14 14:19:08 +0700

    hieu

---
+++ app/src/main/java/com/example/duan1_catmusic/model/casi.java
@@ -0,0 +1,62 @@
+package com.example.duan1_catmusic.model;
+
+public class casi {
+
+    private int MaCaSi;
+
+    private String TenCaSi;
+
+    private String QueQuan;
+
+    private String NamSinh;
+
+    private String HinhCaSi;
+
+    public casi(int maCaSi, String tenCaSi, String queQuan, String namSinh, String hinhCaSi) {
+        MaCaSi = maCaSi;
+        TenCaSi = tenCaSi;
+        QueQuan = queQuan;
+        NamSinh = namSinh;
+        HinhCaSi = hinhCaSi;
+    }
+
+    public int getMaCaSi() {
+        return MaCaSi;
+    }
+
+    public void setMaCaSi(int maCaSi) {
+        MaCaSi = maCaSi;
+    }
+
+    public String getTenCaSi() {
+        return TenCaSi;
+    }
+
+    public void setTenCaSi(String tenCaSi) {
+        TenCaSi = tenCaSi;
+    }
+
+    public String getQueQuan() {
+        return QueQuan;
+    }
+
+    public void setQueQuan(String queQuan) {
+        QueQuan = queQuan;
+    }
+
+    public String getNamSinh() {
+        return NamSinh;
+    }
+
+    public void setNamSinh(String namSinh) {
+        NamSinh = namSinh;
+    }
+
+    public String getHinhCaSi() {
+        return HinhCaSi;
+    }
+
+    public void setHinhCaSi(String hinhCaSi) {
+        HinhCaSi = hinhCaSi;
+    }
+}
