86ef738e91324da03de87dafdf212ffdd6d33e38
Bikerrrr <hieuttps37551@edu.fpt.vn>
2024-07-18 18:25:38 +0700

    bikerr

--- app/src/main/java/com/example/duan1_catmusic/Activity/Screen_nhacTheotheloai.java
+++ app/src/main/java/com/example/duan1_catmusic/Activity/Screen_nhacTheotheloai.java
@@ -14,21 +14,26 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.duan1_catmusic.Adapter.DSplaylistAdapter;
+import com.example.duan1_catmusic.Adapter.TheLoaiAdapter;
 import com.example.duan1_catmusic.Adapter.casiAdapter;
 import com.example.duan1_catmusic.DAO.DSplaylistDAO;
+import com.example.duan1_catmusic.DAO.TheLoaiDAO;
 import com.example.duan1_catmusic.DAO.casiDAO;
 import com.example.duan1_catmusic.R;
 import com.example.duan1_catmusic.model.DSplaylist;
+import com.example.duan1_catmusic.model.TheLoai;
 import com.example.duan1_catmusic.model.casi;
 
 import java.util.ArrayList;
 
 public class Screen_nhacTheotheloai extends AppCompatActivity {
 
-    private RecyclerView rcvlist1,rcvlist;
+    private RecyclerView rcvlist1,rcvlist,rcvtheloai;
     private DSplaylistDAO dSplaylistDAO;
+    private TheLoaiDAO theLoaiDAO;
 
     private ArrayList<DSplaylist> list;
+    private ArrayList<TheLoai> list2;
 
     private ImageView back;
 
@@ -49,6 +54,7 @@ public class Screen_nhacTheotheloai extends AppCompatActivity {
 
         rcvlist1 = findViewById(R.id.rcvlist1);
         rcvlist = findViewById(R.id.rcvlist);
+        rcvtheloai = findViewById(R.id.rcvtheloai);
         back = findViewById(R.id.back);
 
 
@@ -63,13 +69,23 @@ public class Screen_nhacTheotheloai extends AppCompatActivity {
 
         list = new ArrayList<>();
 
+        list2 = new ArrayList<>();
+
         dSplaylistDAO = new DSplaylistDAO(this);
+        theLoaiDAO = new TheLoaiDAO(this);
 
         loadData();
     }
 
     private void loadData(){
         list = dSplaylistDAO.getDSplaylist();
+        list2 = theLoaiDAO.getTheLoai();
+
+
+        GridLayoutManager manager2 = new GridLayoutManager(this, 2);
+        rcvtheloai.setLayoutManager(manager2);
+        TheLoaiAdapter adapter2 = new TheLoaiAdapter(this, list2);
+        rcvtheloai.setAdapter(adapter2);
 
         LinearLayoutManager manager = new LinearLayoutManager(this);
         manager.setOrientation(RecyclerView.HORIZONTAL);
