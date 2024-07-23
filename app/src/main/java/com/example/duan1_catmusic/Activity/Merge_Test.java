2f6274a947728a1ba5a75643c50546211e90258b
Hoang0810 <tuanhaps37570@fpt.edu.vn>
2024-07-23 12:47:37 +0700

    Tun

--- app/src/main/java/com/example/duan1_catmusic/Activity/Merge_Test.java
+++ app/src/main/java/com/example/duan1_catmusic/Activity/Merge_Test.java
@@ -15,7 +15,7 @@ public class Merge_Test extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_merge_test);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
