44d164555b4be50538d1ceacd0b5e5ca30d56d64
soda25042004 <147235001+soda25042004@users.noreply.github.com>
2024-07-24 16:34:00 +0700

    Duong

--- app/src/main/java/com/example/duan1_catmusic/Activity/Screen_QuangCao.java
+++ app/src/main/java/com/example/duan1_catmusic/Activity/Screen_QuangCao.java
@@ -28,13 +28,8 @@ public class Screen_QuangCao extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_screen_quang_cao);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         MobileAds.initialize(this);
 
---
+++ app/src/main/java/com/example/duan1_catmusic/fragment/NghesiAdmin_fm.java
@@ -0,0 +1,47 @@
+package com.example.duan1_catmusic.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.duan1_catmusic.Adapter.thuvienAdapter;
+import com.example.duan1_catmusic.DAO.casiDAO;
+import com.example.duan1_catmusic.R;
+import com.example.duan1_catmusic.model.casi;
+
+import java.util.ArrayList;
+
+
+public class NghesiAdmin_fm extends Fragment {
+
+
+    private RecyclerView rvlistCasi;
+    private com.example.duan1_catmusic.DAO.casiDAO casiDAO;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_nghesi_admin_fm, container, false);
+
+        rvlistCasi = view.findViewById(R.id.rvListCaSi);
+        casiDAO  =new casiDAO(getContext());
+
+        ArrayList<casi> list = casiDAO.getcasi();
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        manager.setOrientation(RecyclerView.VERTICAL);
+        rvlistCasi.setLayoutManager(manager);
+
+        thuvienAdapter adapter = new thuvienAdapter(getContext(), list);
+        rvlistCasi.setAdapter(adapter);
+
+        return view;
+    }
+}
