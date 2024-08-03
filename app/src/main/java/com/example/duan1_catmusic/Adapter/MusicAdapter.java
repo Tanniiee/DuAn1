59399fb76fb5c8e031b49cd3960b33a696c6dc55
soda25042004 <147235001+soda25042004@users.noreply.github.com>
2024-08-03 08:14:19 +0700

    admin nhạc

--- app/src/main/java/com/example/duan1_catmusic/Adapter/MusicAdapter.java
+++ app/src/main/java/com/example/duan1_catmusic/Adapter/MusicAdapter.java
@@ -2,21 +2,23 @@ package com.example.duan1_catmusic.Adapter;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
+import android.content.res.Resources;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.duan1_catmusic.Activity.Screen_listening_music;
 import com.example.duan1_catmusic.R;
 import com.example.duan1_catmusic.model.Nhac;
 
+import java.io.IOException;
 import java.util.List;
 
 public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.ViewHolder> {
@@ -27,6 +29,11 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.ViewHolder> 
     public MusicAdapter(List<Nhac> list, Context context) {
         this.list = list;
         this.context = context;
+    }
+
+    public void updateData(List<Nhac> newList) {
+        this.list = newList;
+        notifyDataSetChanged();  // Cập nhật giao diện người dùng với dữ liệu mới
     }
 
     @NonNull
@@ -40,18 +47,15 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Nhac nhac = list.get(position);
-        holder.tvTenNhac.setText(list.get(position).getTenNhac());
-        holder.tvNgheSi.setText(list.get(position).getTenCaSi());
-        String imgNhac = String.valueOf(list.get(position).getHinhNhac());
-        int resID = ((Activity)context).getResources().getIdentifier(imgNhac,"drawable",((Activity)context).getPackageName());
-        String audioFileName = String.valueOf(list.get(position).getFileNhac());
-        int residnhac = ((Activity)context).getResources().getIdentifier(audioFileName, "raw", ((Activity)context).getPackageName());
+        holder.tvTenNhac.setText(nhac.getTenNhac());
+        holder.tvNgheSi.setText(nhac.getTenCaSi());
 
-
+        // Hiển thị hình ảnh
+        String imgNhac = nhac.getHinhNhac();
+        int resID = ((Activity) context).getResources().getIdentifier(imgNhac, "drawable", ((Activity) context).getPackageName());
         holder.imgNhac.setImageResource(resID);
-        MediaPlayer mediaPlayer = MediaPlayer.create(context, residnhac);
 
-        // Set item click listener
+        // Set click listener for the item
         holder.itemView.setOnClickListener(v -> {
             if (onItemClickListener != null) {
                 int pos = holder.getAdapterPosition();
@@ -60,12 +64,14 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.ViewHolder> 
                 }
             }
         });
+
     }
 
     @Override
     public int getItemCount() {
         return list.size();
     }
+
 
     public interface OnItemClickListener {
         void onItemClick(int position);
